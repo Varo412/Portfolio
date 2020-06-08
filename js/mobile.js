36958840deb24346e25d1a337f5f05e0962f325f
Varo412 <alvarora12@gmail.com>
2020-06-08 17:37:50 +0200

    NavBar improved and mobile support A function detects if the user is using a mobile device so accesibility could be better (hover).

---
+++ js/mobile.js
@@ -0,0 +1,23 @@
+mobileP = {
+  device: null,
+};
+
+mobileM = {
+  mobileInit: function () {
+    mobileP.device = JSON.stringify(mobileM.detectMobile());
+    console.log(mobileP.device);
+  },
+  detectMobile: function () {
+    return (
+      navigator.userAgent.match(/Android/i) ||
+      navigator.userAgent.match(/webOS/i) ||
+      navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/iPod/i) ||
+      navigator.userAgent.match(/BlackBerry/i) ||
+      navigator.userAgent.match(/Windows Phone/i)
+    );
+  },
+};
+
+mobileM.mobileInit();
