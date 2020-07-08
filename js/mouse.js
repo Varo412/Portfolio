991b5a83ec46037c60009c7136b17214dd3f22e1
Varo412 <alvarora12@gmail.com>
2020-07-08 09:14:38 +0200

    Home section advanced: images larger

--- js/mouse.js
+++ js/mouse.js
@@ -3,10 +3,12 @@ mouseP = {
     images: document.querySelectorAll("#mouseEffect img"),
     mouseX: null,
     mouseY: null,
+    motion: null,
 }
 
 mouseM = {
     mouseInit: function () {
+        mouseP.motion = true;
         mouseP.area.addEventListener("mousemove", mouseM.mouseMotion);
         // mouseP.figures.forEach((pic, index) => {
         //     pic.style.zIndex = mouseP.figures.length - index + 1;
@@ -18,10 +20,35 @@ mouseM = {
         mouseP.mouseX = -mouse.offsetX;
         // mouseP.mouseY = -mouse.offsetY;
 
-        mouseP.images.forEach((pic, index) => {
-            pic.style.left = `${mouseP.mouseX / (index * 100 + 50)}%`;
+        // console.log("left", mouseP.images[0].style);
 
-        });
+
+        if (mouseP.mouseX / 50 > -43) { //fin primera imagen
+            mouseP.images.forEach((pic, index) => {
+                var weighing = index * 100 + 50;
+                var value = mouseP.mouseX / weighing;
+
+                // if (value < -43) {
+                //     pic.style.left = "43%";
+                //     mouseP.motion = false;
+                // }
+                // else {
+                //     pic.style.left = `${mouseP.mouseX / weighing}%`;
+                //     mouseP.motion = true;
+                // }
+
+
+                pic.style.left = `${value}%`;
+
+            });
+        }
+
+
+
+
+
+
+
     }
 }
 
