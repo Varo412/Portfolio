98bd9ec219a7c020396a1c72769b08209d89c8ef
Álvaro Rivas <alvarora12@gmail.com>
2020-08-30 13:29:21 +0200

    Initial area (pre)

--- js/mouse.js
+++ js/mouse.js
@@ -3,55 +3,27 @@ mouseP = {
     images: document.querySelectorAll("#mouseEffect img"),
     mouseX: null,
     windowsMotionLimit: window.screen.width * 0.9,
-    motionArea: document.querySelectorAll("#mouseEffect img")[0].width - window.screen.width,
-    initialPosition: 0,
     /* 0.9 is the largest measured value so the functions works fine in most of the devices */
 }
 
 mouseM = {
     mouseInit: function () {
-        console.log("Anchura total", window.screen.width);
-        console.log("límite movimiento (desuso)", mouseP.windowsMotionLimit);
-
-        mouseP.area.addEventListener("mouseenter", mouseM.enterIntoArea);
-
+        mouseP.motion = true;
         mouseP.area.addEventListener("mousemove", mouseM.mouseMotion);
-        console.log("Margen entre display e imagen", mouseP.motionArea);
-
-    },
-
-    enterIntoArea: function (mouse) {
-        mouseP.initialPosition = mouse.offsetX
-        // console.log("entró", mouseP.initialPosition);
-
-        // mouseP.images.forEach((pic, index) => {
-        //     var weighing = index * 100 + 50;
-        //     // var value = (mouseP.initialPosition) / weighing;
-        //     // pic.style.left = `${value}%`;
-        //     pic.style.left = "0";
-        //     // console.log(index, pic.style.left)
-
-        // });
     },
 
     mouseMotion: function (mouse) {
-
-
-        // if (mouse.offsetX < mouseP.motionArea) {
         if (mouse.offsetX < mouseP.windowsMotionLimit) {
-            console.log("Posicion cursor", mouse.offsetX)
 
             mouseP.mouseX = -mouse.offsetX;
 
             mouseP.images.forEach((pic, index) => {
-                // var weighing = index * 100 + 50;
-                var value = (-mouseP.initialPosition + mouse.offsetX) / (index * 100 + 50);
+                var weighing = index * 100 + 50;
+                var value = mouseP.mouseX / weighing;
                 pic.style.left = `${value}%`;
 
             });
         }
-        console.log("left", mouseP.images[0].style.left);
-
     }
 }
 
